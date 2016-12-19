725e3994bbd0f1bfc411564c717fdc0ec1711e34
Boris K <boris.koyfman@gmail.com>
2016-12-18 19:43:15 -0500

    Update and rename program-listing.js to programListing.js

--- js/directives/program-listing.js
+++
@@ -1,9 +0,0 @@
-app.directive('program-listing', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      listing: '=',
-    },
-    templateUrl: 'js/directives/program-listing.html',
-};
-});
